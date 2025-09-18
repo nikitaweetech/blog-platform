cc341f684545eb64ac22d36c9cd3dfd2b0a8e708
Nikita <nikita@weetechsolution.com>
2025-09-18 16:53:56 +0530

    feat: implemented modules with graphql and resolver and entities

--- apps/api/src/post/entities/post.entity.ts
+++ apps/api/src/post/entities/post.entity.ts
@@ -1,4 +1,8 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { Comment } from 'src/comment/entities/comment.entity';
+import { Like } from 'src/like/entities/like.entity';
+import { Tag } from 'src/tag/entities/tag.entity';
+import { User } from 'src/user/entities/user.entity';
 
 @ObjectType() // 👈 This tells GraphQL to expose this class as a schema type
 export class Post {
@@ -25,4 +29,16 @@ export class Post {
 
   @Field()
   updatedAt: Date;
+
+  @Field(() => User)
+  author: User;
+
+  @Field(() => [Comment])
+  comments: Comment[];
+
+  @Field(() => [Tag])
+  tags: Tag[];
+
+  @Field(() => [Like])
+  likes: Like[];
 }
