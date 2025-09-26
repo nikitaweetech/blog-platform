c735880c91708f5b139cff855cfcbcbc13e6d7d7
Nikita <nikita@weetechsolution.com>
2025-09-26 17:23:36 +0530

    feat: created latest posts on home page

---
+++ apps/front/src/components/Posts.tsx
@@ -0,0 +1,19 @@
+import { Post } from "@/lib/types/modelTypes";
+
+type Props={
+    posts:Post
+};
+
+const Posts=(props:Props)=>{
+    return (
+       <section>
+        <h2 className="text-5xl font-bold text-center text-gray-600 leading-tight"> 
+            Latest Posts
+        </h2>
+        <div className="h-1 mx-auto bg-gradient-to-r from-sky-500 to-indigo-500 w-96 mb-9 
+                    rounded-t-md mt-5"></div>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">Post here </div>
+       </section>
+    )
+}
+export default Posts;
