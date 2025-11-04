bbe36d82246e8c526db4da734bca3d026d34d491
Nikita <nikita@weetechsolution.com>
2025-11-04 12:43:02 +0530

    feat: implemented profdile panel on navgitation bar with some options in drop down

--- apps/front/src/components/navbar.tsx
+++ apps/front/src/components/navbar.tsx
@@ -2,6 +2,7 @@ import SignInPage from "@/app/auth/signin/page";
 import { getSession } from "@/lib/session";
 import Link from "next/link";
 import SignInPanel from "./signinPanel";
+import Profile from "./Profile";
 type Props = {};
 
 const Navbar = async(props: Props) => {
@@ -11,11 +12,11 @@ const Navbar = async(props: Props) => {
       <div className="flex-shrink-0">
         <h1 className="text-2xl font-bold">My Modern Blog</h1>
       </div>
-      <div className="flex gap-2 [&>a]:py-2 [&>a]:px-4 [&>a]:transition [&>a]:rounded-md [&>a:hover]:text-sky-100 [&>a:hover]:bg-sky-500">
+      <div className="md:ml-auto  px-2 flex flex-col md:flex-row gap-2 md:items-center md:justify-center [&>a:hover]:bg-sky-500 [&>a:hover]:text-sky-100 [&>a]:rounded-md [&>a]:transition [&>a]:duration-200 [&>a]:px-4 md:[&>a]:py-2 [&>a]:py-1 ">
         <Link href="/">Blog</Link>
         <Link href="#about">About</Link>
         <Link href="#contact">Contact</Link>
-        {session && session.user ?<a href={"/api/auth/signout"}>Sign Out</a>:(<SignInPanel />)}
+        {session && session.user ?(<Profile user={session.user} />):(<SignInPanel />)}
       </div>
     </>
   );
